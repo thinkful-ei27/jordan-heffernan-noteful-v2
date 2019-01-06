873cf113ef4a1f37869810d52cf997be44a73b20
Jordan Heffernan <jheff2@gmail.com>
2019-01-06 11:51:28 -0800

    added frontend functionality for tags

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -17,12 +17,11 @@ $(document).ready(function () {
       noteful.render();
     });
 
-  console.info('Get tags, coming soon...');
-  // api.search('/api/tags')
-  //   .then(response => {
-  //     store.tags = response;
-  //     noteful.render();
-  //   });
+  api.search('/api/tags')
+    .then(response => {
+      store.tags = response;
+      noteful.render();
+    });
 
 });
 
--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -279,12 +279,11 @@ const noteful = (function () {
 
       store.currentNote = {};
 
-      console.info('Get notes by tagId, coming soon...');
-      // api.search('/api/notes', store.currentQuery)
-      //   .then(response => {
-      //     store.notes = response;
-      //     render();
-      //   });
+      api.search('/api/notes', store.currentQuery)
+        .then(response => {
+          store.notes = response;
+          render();
+        });
     });
   }
 
@@ -294,17 +293,16 @@ const noteful = (function () {
 
       const newTagName = $('.js-new-tag-entry').val();
 
-      console.info('Create a tag, coming soon...');
-      // api.create('/api/tags', { name: newTagName })
-      //   .then(() => {
-      //     return api.search('/api/tags');
-      //   }).then(response => {
-      //     store.tags = response;
-      //     render();
-      //   })
-      //   .catch(err => {
-      //     console.error(err);
-      //   });
+      api.create('/api/tags', { name: newTagName })
+        .then(() => {
+          return api.search('/api/tags');
+        }).then(response => {
+          store.tags = response;
+          render();
+        })
+        .catch(err => {
+          console.error(err);
+        });
     });
   }
 
@@ -319,19 +317,18 @@ const noteful = (function () {
 
       store.currentNote = {};
 
-      console.info('Delete a tag, coming soon...');
-      // api.remove(`/api/tags/${tagId}`)
-      //   .then(() => {
-      //     return api.search('/api/tags');
-      //   })
-      //   .then(response => {
-      //     store.tags = response;
-      //     return api.search('/api/notes', store.currentQuery);
-      //   })
-      //   .then(response => {
-      //     store.notes = response;
-      //     render();
-      //   });
+      api.remove(`/api/tags/${tagId}`)
+        .then(() => {
+          return api.search('/api/tags');
+        })
+        .then(response => {
+          store.tags = response;
+          return api.search('/api/notes', store.currentQuery);
+        })
+        .then(response => {
+          store.notes = response;
+          render();
+        });
     });
   }
 
